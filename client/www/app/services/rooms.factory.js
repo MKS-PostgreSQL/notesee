f56f03878bf32d219c87c76eac9ca2f1ccd85c70
Danny Thai <dthai92@yahoo.com>
2016-04-13 19:02:40 -0700

    [bug fix] Implement API call to retrieve classrooms of specified user

--- client/www/app/services/rooms.factory.js
+++ client/www/app/services/rooms.factory.js
@@ -6,9 +6,20 @@
     .factory('Rooms', Rooms)
 
   function Rooms () {
-    return [
-      {name: 'MKS35'},
-      {name: 'HR2'}
-    ]
+    //----- Mock Data ------
+    // return [
+    //   {name: 'MKS35'},
+    //   {name: 'HR2'}
+    // ]
+
+    // retrieve all classroom names for specific user
+    var classRoom = {}
+    classRoom.getClassrooms = getClassrooms
+
+    function getClassrooms (userId) {
+      return $http.get('http://localhost:8080/api/users/user/' + userId + '/classrooms')
+    }
+
+    return classRoom
   }
 })()
