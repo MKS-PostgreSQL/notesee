d15b3ea0d71212946e63c654b303022fc79b73de
Danny Thai <dthai92@yahoo.com>
2016-04-14 13:00:59 -0700

    [Refactor] Retrieve list of classrooms using username

--- Server/Routes/users.js
+++ Server/Routes/users.js
@@ -8,7 +8,52 @@ var db = require('../db.js')
 // users/:id/saved -> return all saved notes user has
 
 
-// retrieve user informaiton details such as full name, username, date of registration given: user id
+
+// retrieve information on all users registered
+/*
+	GET /api/users/
+
+	[
+	  {
+	    "full_name": null,
+	    "id": 2,
+	    "username": "merktassel",
+	    "createdAt": "2016-04-13T22:36:35.000Z",
+	    "email": "merk@tassel.com"
+	  },
+	  {
+	    "full_name": null,
+	    "id": 5,
+	    "username": "spongeypants",
+	    "createdAt": "2016-04-13T23:15:43.000Z",
+	    "email": "crabbypatty@gmail.com"
+	  },
+	  {
+	    "full_name": "Squidward Tentacles",
+	    "id": 8,
+	    "username": "squidster420",
+	    "createdAt": "2016-04-14T21:26:14.000Z",
+	    "email": "squidward@gmail.com"
+	  }
+	]
+*/
+router.get('/', function (req, res) {
+	db.query('SELECT `full_name`, `id`, `username`, `createdAt`, `email` FROM USERS;',
+		function (err, rows) {
+			if (err) {
+				console.error(err)
+				res.status(404).json({success: false})
+			} else {
+				res.json(rows)
+			}
+		})
+})
+
+
+
+
+
+// retrieve user information details such as full name, username, date of registration given: user id
 /* 
 
 	GET /api/users/user/merktassel
@@ -42,7 +87,7 @@ router.get('/user/:name', function (req, res) {
 // retrieve an array of classrooms a user has joined given: user id
 
 /*
-	GET /api/users/user/2/classrooms
+	GET /api/users/user/merktassel/classrooms
 
 	[
 	  {
@@ -52,10 +97,11 @@ router.get('/user/:name', function (req, res) {
 
 */
 
-router.get('/user/:id/classrooms', function (req, res) {
-	var id = req.params.id;
-	db.query('SELECT CLASSROOMS.name FROM CLASSROOMS INNER JOIN CLASSUSERS ON CLASSROOMS.id = CLASSUSERS.classroom_id WHERE CLASSUSERS.user_id = ?;',
-	 [id],
+
+router.get('/user/:name/classrooms', function (req, res) {
+	var username = req.params.name;
+	db.query('SELECT CLASSROOMS.name FROM CLASSROOMS INNER JOIN CLASSUSERS ON CLASSROOMS.id = CLASSUSERS.classroom_id INNER JOIN USERS ON CLASSUSERS.user_id = USERS.id WHERE USERS.username = ?;',
+	 [username],
 	 function (err, rows) {
 		if (err) {
 			console.error(err) 
@@ -65,7 +111,6 @@ router.get('/user/:id/classrooms', function (req, res) {
 		}
 	})
 })
-
 
 
 module.exports = router
