f5a82d949c589c8e51c735c549fb7e8c048e5909
Michelle <mtham101@gmail.com>
2016-04-17 19:31:41 -0700

    [refactor] Allow notes to be uploaded based on Classroom name and username

--- Server/Routes/notes.js
+++ Server/Routes/notes.js
@@ -13,7 +13,6 @@ var base64image = '/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDAAMCAgICAgMCAgIDAwMDBAYEBAQEB
 AWS.config.update({accessKeyId: credentials.accessKeyId, secretAccessKey: credentials.secretAccessKey, region: 'us-west-1'});
 
 router.post('/', function (req, res) {
-  console.log('THIS IS THE REQUEST BODY ', req.body)
   var key = pseudoRandomString();
   console.log(key)
   sendToS3(base64image, key)
@@ -21,35 +20,50 @@ router.post('/', function (req, res) {
   	console.log(data)
   	var attachment = data;
   	var tags = req.body.tags.name
-  	var user_id = req.body.user.id
-  	var classroom_id = req.body.classroom.id
-	db.query('INSERT INTO NOTES SET `attachment` = ?, `user_id` = ?, `classroom_id` = ?;', 
-	  [attachment, user_id, classroom_id], 
-		function (err, result1) {
-		  if(err) {
-		    console.error(err)
-		  } else {
-		    db.query('INSERT INTO TAGS SET `name` = ?;', 
-			  [tags],
-			    function (err, result2) {
-				  if(err) {
-				    console.error(err)
-				  } else {
-				    db.query('INSERT INTO TAGNOTES SET `note_id` = ?, `tag_id` = ?', 
-					  [result1.insertId, result2.insertId],
-					    function (err, result) {
-						  if(err) {
-					        console.error(err)
-							res.status(500).json({success:false})
-						  } else {
-						    res.status(201).json({success:true})
-							console.log('attachment', attachment);
-						}
-				    })
-				}
-		    })
-		}
-	})  
+  	var user = req.body.user.username
+  	var classroom = req.body.classroom.className
+  	db.query('SELECT `id` FROM USERS WHERE `username` = ?;', 
+  		[user], 
+  		function (err, result1) {
+  			if(err) {
+  				console.error(err)
+  			} else {
+  				db.query('SELECT `id` FROM CLASSROOMS WHERE `className` = ?;',
+  					[classroom], 
+  					function (err, result2) {
+  						if(err) {
+  							console.error(err)
+  						} else {
+  							db.query('INSERT INTO NOTES SET `attachment` = ?, `user_id` = ?, `classroom_id` = ?;',
+  								[attachment, result1[0].id, result2[0].id],
+  								function (err, result3) {
+  									if(err) {
+  										console.error(err)
+  									} else {
+  										db.query('INSERT INTO TAGS SET `name` = ?;', 
+  											[tags], 
+  											function (err, result4) {
+  												if(err) {
+  													console.error(err)
+  												} else {
+  													db.query('INSERT INTO TAGNOTES SET `note_id` = ?, `tag_id` = ?;',
+  														[result3.insertId, result4.insertId],
+  														function (err, rows) {
+  															if(err) {
+  																console.error(err)
+  																res.status(500).json({success:false})
+  															} else {
+  																res.status(201).json({success:true})
+  															}
+  														})
+  												}
+  											})
+  									}
+  								})
+  						}
+  					})
+  			}
+  		})
   })
 });
 
