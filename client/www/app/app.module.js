346705b53bfbb3dad936cbf6b271ebfb832e0652
Mark Castle <mark@castle.sx>
2016-04-18 16:50:02 -0700

    Alphabetize Module Dependencies

--- client/www/app/app.module.js
+++ client/www/app/app.module.js
@@ -2,5 +2,5 @@
   'use strict'
 
   angular
-    .module('notesee', ['notesee.core', 'notesee.landing', 'notesee.classrooms', 'notesee.notes', 'notesee.saved', 'notesee.settings'])
+    .module('notesee', ['notesee.core', 'notesee.classrooms', 'notesee.landing', 'notesee.notes', 'notesee.saved', 'notesee.settings'])
 })()
