ec5b6b2b9cde5c3999e194111b42740e5c3d3bec
Danny Thai <dthai92@yahoo.com>
2016-04-17 23:50:31 -0700

    [feat] Verify token for notes route

--- Server/Routes/notes.js
+++ Server/Routes/notes.js
@@ -3,6 +3,7 @@ var router = express.Router()
 var db = require('../db.js')
 var AWS = require('aws-sdk')
 var credentials = require('../credentials.js')
+var auth = require('../auth.js')
 
 function pseudoRandomString() {
     return Math.round((Math.pow(36, 6) - Math.random() * Math.pow(36, 5))).toString(36).slice(1);
@@ -13,63 +14,70 @@ function pseudoRandomString() {
 AWS.config.update({accessKeyId: credentials.accessKeyId, secretAccessKey: credentials.secretAccessKey, region: 'us-west-1'});
 
 router.post('/', function (req, res) {
+	var token = req.headers.token
   var key = pseudoRandomString();
   var base64image = req.body.attachment.base64
-  sendToS3(base64image, key)
-  .then(function(data) {
-  	console.log(data)
-  	var attachment = data;
-  	var tags = req.body.tags.name
-  	var user = req.body.user.username
-  	var classroom = req.body.classroom.className
-  	db.query('SELECT `id` FROM USERS WHERE `username` = ?;', 
-  		[user], 
-  		function (err, result1) {
-  			if(err) {
-  				console.error(err)
-  			} else {
-  				db.query('SELECT `id` FROM CLASSROOMS WHERE `className` = ?;',
-  					[classroom], 
-  					function (err, result2) {
-  						if(err) {
-  							console.error(err)
-  						} else {
-  							if (result1[0] === undefined || result2[0] === undefined) {
-  							  	res.status(500).json({success:false});
-  							  } else {
-  							  	db.query('INSERT INTO NOTES SET `attachment` = ?, `user_id` = ?, `classroom_id` = ?;',
-  								[attachment, result1[0].id, result2[0].id],
-  								function (err, result3) {
-  									if(err) {
-  										console.error(err)
-  									} else {
-  										db.query('INSERT INTO TAGS SET `name` = ?;', 
-  											[tags], 
-  											function (err, result4) {
-  												if(err) {
-  													console.error(err)
-  												} else {
-  													db.query('INSERT INTO TAGNOTES SET `note_id` = ?, `tag_id` = ?;',
-  														[result3.insertId, result4.insertId],
-  														function (err, rows) {
-  															if(err) {
-  																console.error(err)
-  																res.status()
-  															} else {
-  																res.status(201).json({success:true})
-  															}
-  														})
-  												}
-  											})
-  									}
-  								})
-  							  }
-  							
-  						}
-  					})
-  			}
-  		})
-  })
+  var postNote = function () {
+	  sendToS3(base64image, key)
+	  .then(function(data) {
+	  	console.log(data)
+	  	var attachment = data;
+	  	var tags = req.body.tags.name
+	  	var user = req.body.user.username
+	  	var classroom = req.body.classroom.className
+	  	db.query('SELECT `id` FROM USERS WHERE `username` = ?;', 
+	  		[user], 
+	  		function (err, result1) {
+	  			if(err) {
+	  				console.error(err)
+	  			} else {
+	  				db.query('SELECT `id` FROM CLASSROOMS WHERE `className` = ?;',
+	  					[classroom], 
+	  					function (err, result2) {
+	  						if(err) {
+	  							console.error(err)
+	  						} else {
+	  							if (result1[0] === undefined || result2[0] === undefined) {
+	  							  	res.status(500).json({success:false});
+	  							  } else {
+	  							  	db.query('INSERT INTO NOTES SET `attachment` = ?, `user_id` = ?, `classroom_id` = ?;',
+	  								[attachment, result1[0].id, result2[0].id],
+	  								function (err, result3) {
+	  									if(err) {
+	  										console.error(err)
+	  									} else {
+	  										db.query('INSERT INTO TAGS SET `name` = ?;', 
+	  											[tags], 
+	  											function (err, result4) {
+	  												if(err) {
+	  													console.error(err)
+	  												} else {
+	  													db.query('INSERT INTO TAGNOTES SET `note_id` = ?, `tag_id` = ?;',
+	  														[result3.insertId, result4.insertId],
+	  														function (err, rows) {
+	  															if(err) {
+	  																console.error(err)
+	  																res.status()
+	  															} else {
+	  																res.status(201).json({success:true})
+	  															}
+	  														})
+	  												}
+	  											})
+	  									}
+	  								})
+	  							  }
+	  							
+	  						}
+	  					})
+	  			}
+	  		})
+	  })
+  }
+  var error = function () {
+  	res.status(404).json({success: false, tokenValid: false})
+  }
+  auth.verifyToken(token, postNote, error)
 });
 
 function sendToS3 (img, key) {
