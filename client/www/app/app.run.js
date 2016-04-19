8450ad287b0bd93e18ddf6b1f41b60372de528a4
Mark Castle <mark@castle.sx>
2016-04-19 10:12:08 -0700

    Finalize Miscellaneous

--- client/www/app/app.run.js
+++ client/www/app/app.run.js
@@ -5,9 +5,30 @@
     .module('notesee')
     .run(run)
 
-  function run ($state) {
+  function run (Auth, $http, $ionicPlatform, $state) {
+    // Configure Ionic Platform
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true)
+        cordova.plugins.Keyboard.disableScroll(true)
+      }
 
-  	// Load Classrooms Page as Initial State
-  	$state.go('tab.classrooms')
+      if (window.StatusBar) {
+        StatusBar.styleDefault()
+      }
+    })
+
+    // Set Headers & Load Classrooms Page If Logged In
+    if (Auth.current().token) {
+      // Append Authentication Information to Each HTTP Request
+      $http.defaults.headers.common['username'] = Auth.current().username
+      $http.defaults.headers.common['token'] = Auth.current().token
+
+      // Go To Classrooms Page
+      $state.go('tab.classrooms')
+    } else {
+      // Otherwise Go To Landing Page
+      $state.go('landing')
+    }
   }
 })()
