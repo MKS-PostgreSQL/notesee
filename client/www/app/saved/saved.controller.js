a3925751b28a51c3f31b4e3371ee483f0623b87f
Mark Castle <mark@castle.sx>
2016-04-19 10:10:38 -0700

    Finalize Saved

--- client/www/app/saved/saved.controller.js
+++ client/www/app/saved/saved.controller.js
@@ -5,7 +5,21 @@
     .module('notesee.saved')
     .controller('SavedController', SavedController)
 
-  function SavedController () {
+  function SavedController (Auth, Notes) {
+    // Initialization
     var vm = this
+    activate()
+
+    // Variables
+    vm.notes = []
+
+    // Functions
+
+    // Implementation Details
+    function activate () {
+      Notes.saved(Auth.current().username).success(function (data) {
+        vm.notes = data
+      })
+    }
   }
 })()
